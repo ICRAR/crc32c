50eee9f4dfc64920e33b32c29dafa5416cd17fe3
Rodrigo Tobar <rtobar@icrar.org>
2018-07-16 17:26:10 +0800

    Removing stream consumption methods
    These were always *very* experimental, and it doesn't make sense to keep
    them here really.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- _crc32c.c
+++ _crc32c.c
@@ -32,7 +32,6 @@
 
 #include "checksse42.h"
 #include "common.h"
-#include "consume.h"
 #include "crc32c.h"
 
 /* Python 3 doesn't have PyInt anymore */
@@ -40,65 +39,6 @@
 	#define PyInt_FromLong PyLong_FromLong
 #endif
 
-#ifndef _IS_WINDOWS
-static
-PyObject* crc32c_consume(PyObject *self, PyObject *args) {
-
-	int fd_in, fd_out;
-	unsigned long buffsize;
-	unsigned long total;
-	unsigned short crc_type;
-	uint32_t crc;
-	int stat;
-	float timeout;
-	unsigned long crc_time = 0;
-	unsigned long write_time = 0;
-	Py_buffer pbin;
-	unsigned char *buff_data;
-
-	if (!PyArg_ParseTuple(args, "is*ifkkH", &fd_in, &pbin, &fd_out, &timeout, &buffsize, &total, &crc_type) )
-		return NULL;
-
-	buff_data = pbin.buf;
-	crc = _crc32c_intel(0U, buff_data, pbin.len);
-	stat = write(fd_out, buff_data, pbin.len);
-	if( stat == -1 || stat != pbin.len ) {
-		PyBuffer_Release(&pbin);
-		char *error = strerror(errno);
-		char *fmt = "Error while writing initial data: %s";
-		char *msg = (char *)malloc(strlen(error) + strlen(fmt) - 1);
-		sprintf(msg, fmt, error);
-		PyErr_SetString(PyExc_Exception, msg);
-		free(msg);
-		return NULL;
-	}
-	total -= pbin.len;
-	PyBuffer_Release(&pbin);
-
-	Py_BEGIN_ALLOW_THREADS
-	stat = _crc32c_read_crc_write(fd_in, fd_out, timeout, buffsize, total, crc_type, &crc, &crc_time, &write_time);
-	Py_END_ALLOW_THREADS
-
-	if( stat ) {
-		stat = -1*stat - 1;
-		char *error = strerror(errno);
-		char *action[] = {"preparing to loop", "reading", "writing", "completing writing"};
-		char *fmt = "Error while %s: %s";
-		char *msg = (char *)malloc(strlen(error) + strlen(fmt) + strlen(action[stat]) - 1);
-		sprintf(msg, fmt, action[stat], error);
-		PyErr_SetString(PyExc_Exception, msg);
-		free(msg);
-		return NULL;
-	}
-
-
-	PyObject* res = PyTuple_New(3);
-	PyTuple_SetItem(res, 0, PyInt_FromLong(crc));
-	PyTuple_SetItem(res, 1, PyInt_FromLong(crc_time));
-	PyTuple_SetItem(res, 2, PyInt_FromLong(write_time));
-	return res;
-}
-#endif // _IS_WINDOWS
 
 static
 PyObject* crc32c_crc32(PyObject *self, PyObject *args) {
@@ -130,9 +70,6 @@ PyObject* crc32c_crc32(PyObject *self, PyObject *args) {
 
 static PyMethodDef CRC32CMethods[] = {
 	{"crc32",   crc32c_crc32,   METH_VARARGS, "Calculate crc32c using Intel SSE4.2 instruction."},
-#ifndef _IS_WINDOWS
-	{"consume", crc32c_consume, METH_VARARGS, "read/crc/write loop using Intel SSE4.2 instruction"},
-#endif // _IS_WINDOWS
 	{NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from setuptools import setup, Extension
 crcmod_ext = Extension('crc32c',
                        depends=glob.glob('*.h'),
                        language='c',
-                       sources=['_crc32c.c', 'crc32c.c', 'checksse42.c', 'consume.c'],
+                       sources=['_crc32c.c', 'crc32c.c', 'checksse42.c'],
                        include_dirs=['.'])
 
 classifiers = [
