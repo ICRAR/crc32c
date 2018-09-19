bd1930830efbd9b759b30a2cfa3d9f519f3c3c02
Rodrigo Tobar <rtobar@icrar.org>
2018-09-19 17:22:01 +0800

    More knowledge in common.h
    We are now adding detection of 32/64 bits systems here since it will be
    handy in the future to have it centralized, a definition for uint16_t if
    no stdint.h header is available, and a macro to inline functions safely
    depending on the compiler we use.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- common.h
+++ common.h
@@ -26,12 +26,28 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
-/* MSVC starting include stdint.h fairly recently */
+/* inline support (it's not a keyword in MSVC for C code) */
+#if defined(_MSC_VER)
+# define CRC32C_INLINE __inline
+#else
+# define CRC32C_INLINE inline
+#endif
+
+/* 32/64 bit detection */
+#if defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64) ||\
+    (defined(_MSC_VER) && defined(_M_AMD64))
+# define CRC32C_IS_64_BITS
+#else
+# undef CRC32C_IS_64_BITS
+#endif
+
+/* uint64_t / uint32_t / uint16_t definitions */
 #if !defined(_MSC_VER) || (MSC_VER >= 1800)
 # include <stdint.h>
 #else
 # include <limits.h>
   typedef unsigned __int64 uint64_t;
+
 # if ULONG_MAX == (0xffffffffUL)
    typedef unsigned long uint32_t;
 # elif UINT_MAX == (0xffffffffUL)
@@ -39,11 +55,21 @@
 # else
 #  error "Unsupported platform"
 # endif
+
+# if UINT_MAX == (0xffffUL)
+   typedef unsigned int uint16_t;
+# elif USHRT_MAX == (0xffffUL)
+   typedef unsigned short uint16_t;
+# else
+#  error "Unsupported platform"
+# endif
+
 #endif
 
-/* Definition of size_t */
+/* size_t definition */
 #include <stdlib.h>
 
+/* crc32c function signature */
 typedef uint32_t (* crc_function)(uint32_t crc, unsigned const char *data, unsigned long length);
 
 #endif // _COMMON_H_
