ea478f20875fe3de6e1c98432f1b82c3334dc2c9
Rodrigo Tobar <rtobar@icrar.org>
2018-09-20 11:50:40 +0800

    Adding python 3.7 to list of classifiers
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ classifiers = [
     "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
 ]
 
 with open('README.rst', 'rt') as f:
