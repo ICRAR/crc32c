ICRAR/crc32c

commit 21a464c0923595ff5c2e3e29a9bc42f98cd2b60d

Fix compilation on non-Intel platforms (fixes: #6)

setup.py
@@ -20,6 +20,7 @@
 #    MA 02111-1307  USA
 #
 import glob
+import platform
 
 import distutils.ccompiler
 from setuptools import setup, Extension
@@ -27,24 +28,31 @@ from setuptools.command.build_ext import build_ext
 
 
 crcmod_ext = Extension('crc32c',
+                       define_macros=[('NDEBUG', None)],
                        depends=glob.glob('*.h'),
                        language='c',
-                       sources=['_crc32c.c', 'crc32c_adler.c', 'crc32c_sw.c', 'checksse42.c'],
+                       sources=['_crc32c.c', 'crc32c_sw.c'],
                        include_dirs=['.'])
+is_intel = platform.machine() in ['x86_64', 'AMD64']
 
 def get_extra_compile_args():
     # msvc is treated specially; otherwise we assume it's a unix compiler
     comp = distutils.ccompiler.get_default_compiler()
     if comp == 'msvc':
-        return ['/O2', '/DNDEBUG']
+        return ['/O2']
+    elif is_intel:
+        return ['-O3', '-msse4.2', '-mpclmul']
     else:
-        return ['-O3', '-msse4.2', '-mpclmul', '-DNDEBUG']
+        return ['-O3']
 
 class _build_ext(build_ext):
     """Custom build_ext command that includes extra compilation arguments"""
     def run(self):
         assert(len(self.distribution.ext_modules) == 1)
         self.distribution.ext_modules[0].extra_compile_args = get_extra_compile_args()
+        if is_intel:
+            self.distribution.ext_modules[0].define_macros += [('IS_INTEL', None)]
+            self.distribution.ext_modules[0].sources += ['crc32c_adler.c', 'checksse42.c']
         build_ext.run(self)
 
 

_crc32c.c
@@ -98,7 +98,6 @@ MOD_INIT(crc32c)
 	PyObject *m;
 
 	char *sw_mode = getenv("CRC32C_SW_MODE");
-	int has_hw_impl = _crc32c_intel_probe();
 	int force_sw_impl = sw_mode != NULL && (!strcmp(sw_mode, "1") || !strcmp(sw_mode, "force"));
 	int fallback_to_sw_impl = sw_mode != NULL && !strcmp(sw_mode, "auto");
 
@@ -106,10 +105,12 @@ MOD_INIT(crc32c)
 	if (force_sw_impl) {
 		crc_fn = _crc32c_sw_slicing_by_8;
 	}
-	else if (has_hw_impl) {
+#if defined(IS_INTEL)
+	else if (_crc32c_intel_probe()) {
 		crc_fn = _crc32c_hw_adler;
 		crc32c_init_hw_adler();
 	}
+#endif
 	else if (fallback_to_sw_impl) {
 		crc_fn = _crc32c_sw_slicing_by_8;
 	}

commit d8f243accc7db2505c603077fc23d1001e313165

Declare all Extension sources unconditionally
The dynamic nature in which sources were added to the Extension object
at build time meant that the extra sources (crc32_adler.c and
checksse42.c) were not included in source distributions produced via the
"sdist" setup.py command (because the "build" command does not run as
part of "sdist"). This in turn meant that trying to install such source
distributions in Intel platforms failed because the building process
failed to find the missing files in the source distributions.

This problem was introduced in 21a464c but went unnoticed until now
because we have been producing source distributions locally using the
exact same environment for years, before that particular change was
introduced (and therefore our local copy of the MANIFEST already
contained the two files as part of the distribution). Now that we are
moving towards producing all distribution files in GitHub via its
Actions functionality we stumbled upon this particular issue.

Fixing this problem involves adding back the two source files to be part
of the Extension unconditionally, and instead add the proper macro
checks to include the file's contents conditionally depending on the
target platform.

Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

checksse42.c
@@ -23,6 +23,8 @@
  *
  */
 
+#if defined(IS_INTEL)
+
 #if defined(_MSC_VER)
 #include <intrin.h>
 
@@ -47,3 +49,5 @@ int _crc32c_intel_probe(void) {
 	return (ecx & (1 << 20)) != 0;
 }
 #endif // defined(_MSC_VER)
+
+#endif // defined(IS_INTEL)

crc32c_adler.c
@@ -43,6 +43,7 @@
  *                              - Moved crc32q into macro's and put alternative code there for 32bit operation
 */
 
+#if defined(IS_INTEL)
 
 #include "common.h"
 
@@ -342,3 +343,5 @@ uint32_t _crc32c_hw_adler(uint32_t crc, const unsigned char *buf, unsigned long 
         }
         return ( uint32_t ) crc32bit;
 }
+
+#endif // defined(IS_INTEL)

setup.py
@@ -31,7 +31,7 @@ crcmod_ext = Extension('crc32c',
                        define_macros=[('NDEBUG', None)],
                        depends=glob.glob('*.h'),
                        language='c',
-                       sources=['_crc32c.c', 'crc32c_sw.c'],
+                       sources=['_crc32c.c', 'checksse42.c', 'crc32c_adler.c', 'crc32c_sw.c'],
                        include_dirs=['.'])
 
 def get_extra_compile_args(is_intel):
@@ -60,7 +60,6 @@ class _build_ext(build_ext):
         self.distribution.ext_modules[0].extra_compile_args = get_extra_compile_args(is_intel)
         if is_intel:
             self.distribution.ext_modules[0].define_macros += [('IS_INTEL', None)]
-            self.distribution.ext_modules[0].sources += ['crc32c_adler.c', 'checksse42.c']
         build_ext.run(self)
 
 