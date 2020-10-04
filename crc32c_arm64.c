d86a80ac896146dff3cd30131e7495dc4fd4f1a4
Rodrigo Tobar <rtobar@icrar.org>
2020-10-04 09:33:33 +0800

    Reference correct files in source code
    Now that we have our own copies of the original LICENSE/AUTHOR files
    from the projects we have taken code from, we can further modify our
    copies of the source code to point to our copies of these files.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- crc32c_arm64.c
+++ crc32c_arm64.c
@@ -1,6 +1,7 @@
 // Copyright 2017 The CRC32C Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file. See the AUTHORS file for names of contributors.
+// found in the LICENSE.google-crc32c file. See the AUTHORS.google-crc32c file
+// for names of contributors.
 
 // In a separate source file to allow this accelerated CRC32C function to be
 // compiled with the appropriate compiler flags to enable ARM NEON CRC32C
--- crc32c_sw.c
+++ crc32c_sw.c
@@ -1,6 +1,6 @@
 // Copyright 2008,2009,2010 Massachusetts Institute of Technology.
 // All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// BSD-style license that can be found in the LICENSE.slice-by-8 file.
 
 // Implementations adapted from Intel's Slicing By 8 Sourceforge Project
 // http://sourceforge.net/projects/slicing-by-8/
